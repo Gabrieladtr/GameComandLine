0799ae35ea68d74b2a153176e480e80de1dca1de
Gabrieladtr <118777502+Gabrieladtr@users.noreply.github.com>
2023-08-09 01:50:49 -0300

    V3 - Lógica do player 2 criada
    Podemos competir contra o player agora, mas consegue comprar apenas uma carta para revidar. Entao ele nao consegue revidar a menos que tenhamos menos de 13 pontos.
    
    Ou seja, assim que eu consertar essa logica entao o player 2 estara funcionando bem

--- BlackJack.cs
+++ BlackJack.cs
@@ -8,7 +8,7 @@ namespace BlackJackGame
         public static void PlayBlackJack()
         {
             bool executando = true, executandoComprarCartas = true;
-            int carta1PlayerDois, carta2PlayerDois, somaPlayerDois;
+            int carta1PlayerDois, carta2PlayerDois, somaPlayerDois, newCardPlayerDois = 0;
             int carta1PlayerUm, carta2PlayerUm, somaPlayerUm, newCardPlayerUm;
             Random random = new Random();
 
@@ -48,9 +48,22 @@ namespace BlackJackGame
 
                 while(executandoComprarCartas == true)
                 {
-                    mostrarMenuDeAcoes();
+                    
+                    
+                    bool player2Comprando = true;//define se o player 2 esta comprando cartas no momento
+
+                    if(player2Comprando == false)
+                    {
+                        mostrarMenuDeAcoesTermino();//Vai mostrar as opcoes de fim de jogo
+                    }else
+                    {
+                        mostrarMenuDeAcoes();//Vai mostrar as opcoes a cada rodada
+                    }
+
                     //O player vai decidir o que fazer
                     decisaoPlayerUm = Console.ReadLine();
+
+
 
                     switch(decisaoPlayerUm)
                     {
@@ -74,9 +87,16 @@ namespace BlackJackGame
 
                                     }else if(somaPlayerUm == 21)
                                     {
-                                        Console.WriteLine("Vc tem 21. Temos que calcular se ganhou ou perdeu");
+                                        if(somaPlayerDois < somaPlayerUm)
+                                        {
+                                            Console.WriteLine("Vc tem 21. Vc ganhou!");
+                                            
+                                        }
+                                        
+                                        Console.WriteLine("Finalizando o jogo.");
                                         executandoComprarCartas = false;
                                         executando = false;
+                                        //Vai finalizar o game
                                         
                                     } else if (somaPlayerUm < 21)
                                     {
@@ -89,9 +109,117 @@ namespace BlackJackGame
                         break;
                         case "2":
                             //Vc vai passar a rodada, deixando o player 2 decidir.
-                            //Aqui devera ficar um robô que entende apenas a primeira carta do player 1
+                            //Esse robô vai entender quanto no total o player 2 tem e deve tentar estar
+                            //com mais pontos do que ele e tmb nao ultrapassar o 21.
+                            //ou seja somaPlayerUm <= 21 && somaPlayerDois <= 21
                             //E tenta criar o melhor deck possível.
-                            Console.WriteLine("Estamos em 2");
+                            Console.WriteLine("Voce desistiu. Agora é a vez do player 2.");
+                            Console.WriteLine("");
+                            
+
+                            
+                            while(player2Comprando)
+                            {
+                                
+                                    //vai comprar mais uma carta
+                                    
+                                    if(somaPlayerDois <= 11)
+                                    {
+                                        if(somaPlayerUm > somaPlayerDois)
+                                        {
+                                            newCardPlayerDois = random.Next(1,11);//Vai gerar a nova carta da rodada
+                                            somaPlayerDois = (somaPlayerDois + newCardPlayerDois);//Vai armanezar todas as cartas anteriores
+                                            Console.WriteLine("Jogador 2 | Carta um: " + carta1PlayerDois + " | Carta dois: " + carta2PlayerDois + " | Nova carta: " + newCardPlayerDois + " | Total: " + somaPlayerDois);
+                                            Console.WriteLine("");
+                                        }else
+                                        {
+                                            //provavelmente estamos ganhando, entao paramos por aqui.
+                                            player2Comprando = false;
+                                        }
+
+                                    }else if(somaPlayerDois >=12 && somaPlayerDois <=16)
+                                    {
+                                        if(somaPlayerUm > somaPlayerDois)
+                                        {
+                                            newCardPlayerDois = random.Next(1,11);//Vai gerar a nova carta da rodada
+                                            somaPlayerDois = (somaPlayerDois + newCardPlayerDois);//Vai armanezar todas as cartas anteriores
+                                            Console.WriteLine("Jogador 2 | Carta um: " + carta1PlayerDois + " | Carta dois: " + carta2PlayerDois + " | Nova carta: " + newCardPlayerDois + " | Total: " + somaPlayerDois);
+                                            Console.WriteLine("");
+                                        }else
+                                        {
+                                            //provavelmente estamos ganhando, entao paramos por aqui.
+                                            player2Comprando = false;
+                                        }
+
+                                    }else if(somaPlayerDois >=17 && somaPlayerDois <=20)
+                                    {
+                                        //So vai comprar cartas novas se o player 1 estiver com mais pontos
+                                        //Entao o sistema vai tentar entender quanto o jogador 1 tem e vai tentar comprar
+                                        //novas cartas ate conseguir superar o jogador 1
+                                        if(somaPlayerUm > somaPlayerDois)
+                                        {
+                                            newCardPlayerDois = random.Next(1,11);//Vai gerar a nova carta da rodada
+                                            somaPlayerDois = (somaPlayerDois + newCardPlayerDois);//Vai armanezar todas as cartas anteriores
+                                            Console.WriteLine("Jogador 2 | Carta um: " + carta1PlayerDois + " | Carta dois: " + carta2PlayerDois + " | Nova carta: " + newCardPlayerDois + " | Total: " + somaPlayerDois);
+                                            Console.WriteLine("");
+                                        }else
+                                        {
+                                            //provavelmente estamos ganhando, entao paramos por aqui.
+                                            player2Comprando = false;
+                                        }
+
+
+                                    //ate aqui vai controlar a compra de cartas
+                                    //A partir daqui vai calcular o que fazer de acordo com os pontos
+                                    }
+                                    
+                                    if(somaPlayerDois > 21 && somaPlayerUm <= 21){
+
+                                        Console.WriteLine("O jogador 2 estourou o deck. Voce ganhou.");
+                                        player2Comprando = false;
+                                        Console.WriteLine("Finalizando o jogo.");
+                                        executandoComprarCartas = false;
+                                        executando = false;
+                                        //Vai finalizar o game
+                                    }
+                                    else if(somaPlayerDois == 21)
+                                    {
+                                        if(somaPlayerDois == somaPlayerUm)
+                                        {
+                                            Console.WriteLine("Empate!");
+                                            player2Comprando = false;
+                                            Console.WriteLine("Finalizando o jogo.");
+                                            executandoComprarCartas = false;
+                                            executando = false;
+                                            //Vai finalizar o game
+                                        } 
+                                        
+                                    }else if(somaPlayerDois > somaPlayerUm)
+                                    {
+                                        Console.WriteLine("Player 2 ganhou!");
+                                        Console.WriteLine("Jogador 2 | Carta um: " + carta1PlayerDois + " | Carta dois: " + carta2PlayerDois + " | Nova carta: " + newCardPlayerDois + " | Total: " + somaPlayerDois);
+
+                                        
+                                        player2Comprando = false;
+                                        Console.WriteLine("Finalizando o jogo.");
+                                        executandoComprarCartas = false;
+                                        executando = false;
+                                        //Vai finalizar o game
+
+                                    }else if(somaPlayerUm > somaPlayerDois)
+                                    {
+                                        Console.WriteLine("Player 1 ganhou!");
+                                        player2Comprando = false;
+                                        Console.WriteLine("Finalizando o jogo.");
+                                        executandoComprarCartas = false;
+                                        executando = false;
+                                        //Vai finalizar o game
+                                    }
+
+
+                                    
+                            }
+
 
                         break;
                         case "3":
@@ -127,7 +255,25 @@ namespace BlackJackGame
             Console.WriteLine("Aperte alguma das teclas para continuar");
             Console.WriteLine("");
         }
+
+        public static void mostrarMenuDeAcoesTermino()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("4 - Sair do jogo");
+            Console.WriteLine("2 - Recomeçar outro jogo");
+            Console.WriteLine("Aperte alguma das teclas para continuar");
+            Console.WriteLine("");
+        }
         
+        public static void DecideJogoPlayer2(int somaPlayer1, int somaPlayer2, bool decisaoPlayerDoisV2)
+        {
+            //Calculara a jogada do player 2.    
+
+            
+
+
+        }
 
     }
 }
